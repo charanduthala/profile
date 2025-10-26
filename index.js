be4a49ad4562178705c171ceda4bc8a035203828
charan Duthala <duthalacharantejareddy@gmail.com>
2025-10-26 20:13:46 +0530

    added free icons

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const menu = document.getElementById('menu');
 const menuContainer = document.getElementById('menu-container')
+const downloadCv = document.getElementById('downloadCV')
+const linkedIn = document.getElementById('linkedIn');
+const email = document.getElementById('email')
+const copyright = document.getElementById('copyright')
 
 
 
@@ -25,4 +29,26 @@ function closeMenu() {
     menu.querySelector('.menu').classList.remove('open')
 }
 
+function downloadResume() {
+    window.open('./public/D CHARAN TEJA REDDY.pdf')
+}
+
+function openLinkedin() {
+    window.open('https://www.linkedin.com/in/charan-teja-reddy-duthala/')
+}
+
+function sendEmail() {
+    window.location.href = "mailto:duthalacharantejareddy@gmail.com";
+}
+
 backdrop.addEventListener("click", closeMenu);
+
+downloadCv.addEventListener('click', downloadResume)
+
+linkedIn.addEventListener('click', openLinkedin);
+email.addEventListener('click', sendEmail)
+
+const date = new Date()
+const year = date.getFullYear();
+
+copyright.textContent = "© " + year + " Charan Teja Reddy Duthala"
